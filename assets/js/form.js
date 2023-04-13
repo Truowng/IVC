6a85343e6fc42aeeb60edf9d4fcd4a6e97c3e05f
Truowng <quockhanhtruong166@gmail.com>
2023-04-13 10:08:30 +0700

    add handleValue func

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,12 +1,20 @@
 const modal = document.querySelector("#modal");
+const loanInput = document.querySelector('[name="sotienvay"]');
 
 const clearValue = (name) => {
   if (name == "legion") {
     document.querySelector("[list='legion']").value = "";
   }
-  if (name == "incomeType") {
+  if (name == "income-type") {
     document.querySelector("#income-type").value = "Vui lòng chọn";
   }
+};
+
+const handleValue = (value) => {
+  let result = Number(value).toLocaleString("en-US");
+  result === "NaN"
+    ? (loanInput.value = "Vui lòng nhập số")
+    : (loanInput.value = result);
 };
 
 const onSubmit = (event) => {
