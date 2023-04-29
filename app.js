63856a1306c601c8ffc5f39c87a94ad04cd6a6c3
Do Duy Anh <duyanhvn1999@gmail.com>
2023-04-29 16:46:11 +0700

    chore: change mail secure config

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/contact', async (req, res) => {
     const mailConfig = {
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
